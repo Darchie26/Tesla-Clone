3c7e3ae610ea11d38ebbae39f8402d9d3cdc7c1b
Deonte Archie <45987095+Deontearchie@users.noreply.github.com>
2021-07-06 02:07:17 -0400

    Added a side bar where the user can select the optiion they are looking for. They also can click thee hamburger and it will open the side bar.

--- tesla-clone/src/components/Header.js
+++ tesla-clone/src/components/Header.js
@@ -1,8 +1,13 @@
-import React from 'react'
+import React, {useState} from 'react'
 import styled from "styled-components"
 import MenuIcon from '@material-ui/icons/Menu'
+import CloseIcon from '@material-ui/icons/Close';
+
 
 function Header() {
+    const [burgerStatus, setBurgerStatus] = useState(false);
+    
+   
     return (
         <Container>
             <a>
@@ -14,11 +19,24 @@ function Header() {
                 <p><a href="#">Model X</a></p>
                 <p><a href="#">Model Y</a></p>
             </Menu>
-            <RighhtMenu>
+            <RightMenu>
                 <a href="#">Shop</a>
                 <a href="#">Tesla Account</a>
-                <CustomMenu/>
-            </RighhtMenu>
+                <CustomMenu onClick={()=>setBurgerStatus(true)}/>
+            </RightMenu>
+            <BurgerNav show={burgerStatus}>
+                <CloseWrapper>
+                <CustomClose onClick={()=>setBurgerStatus(false)}/>
+                </CloseWrapper>
+                <li><a href="#">Model S</a></li>
+                <li><a href="#">Model Y</a></li>
+                <li><a href="#">Model 3</a></li>
+                <li><a href="#">Model X</a></li>
+                <li><a href="#">Solar Panels</a></li>
+                <li><a href="#">Existing Inventory</a></li>
+                <li><a href="#">Trade In</a></li>
+                <li><a href="#">Shop</a></li>
+            </BurgerNav>
         </Container>
     )
 }
@@ -30,10 +48,12 @@ min-height: 60px;
 position: fixed;
 display: flex;
 align-items:center;
+justify-content: space-between;
 padding: 0 20px;
 top:0;
 left:0;
 right:0;
+z-index: 1;
 `
 
 
@@ -50,8 +70,13 @@ a{
     padding: 0 10px;
     flex-wrap: nowrap;
 }
+ @media(max-width: 768px) {
+     display: none;
+ }
 `
-const RighhtMenu = styled.div`
+const RightMenu = styled.div`
+display: flex;
+align-items: center;
 a{
     font-weight:600;
     text-transform:uppercase;
@@ -62,4 +87,40 @@ a{
 
 const CustomMenu = styled(MenuIcon)`
 cursor: pointer;
+
+`
+
+const BurgerNav = styled.div`
+position: fixed;
+top:0;
+bottom:0;
+right:0;
+background:white;
+width:300px;
+z-index: 16;
+list-style: none;
+padding: 20px;
+display: flex;
+flex-direction: column;
+text-align: start;
+transform: ${props => props.show ? 'translateX(0)':'translateX(100%)'};
+transition: transform 0.2s ease-in;
+li{
+    padding:15px 0;
+    border-bottom: 1px solid rgba(0, 0, 0, .2);
+
+    a{
+        font-weight: 600;
+    }
+}
+`
+
+const CustomClose = styled(CloseIcon)`
+cursor: pointer;
+`
+
+
+const CloseWrapper = styled.div`
+display: flex;
+justify-content: flex-end;
 `
